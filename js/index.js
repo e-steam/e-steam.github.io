559913c714e4179985187785bac8b5106ff64182
Tom Wagg <twagg@college.harvard.edu>
2023-04-06 18:43:07 -0400

    add a lot more templating

--- js/index.js
+++ js/index.js
@@ -1,3 +1,8 @@
 window.addEventListener("load", function() {
     window.location.href = "construction.html"
 })
+
+const containers = document.querySelectorAll(".slanted-container");
+for (let i = 0; i < containers.length; i++) {
+    containers[i].style.transform = "translateY(calc(-0.25 * " + i.toString() + "* var(--skewness))";
+}
