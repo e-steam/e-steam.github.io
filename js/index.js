41547ee4871722b2edf9a362d72409110030cb63
Tom Wagg <twagg@college.harvard.edu>
2023-04-06 19:14:17 -0400

    finish templating about us

--- js/index.js
+++ js/index.js
@@ -6,3 +6,10 @@ const containers = document.querySelectorAll(".slanted-container");
 for (let i = 0; i < containers.length; i++) {
     containers[i].style.transform = "translateY(calc(-0.25 * " + i.toString() + "* var(--skewness))";
 }
+
+const names = ["Albert Einstein", "Annie Jump-Cannon", "Jocelyn Bell-Burnell", "Edwin Hubble"]
+let i = 0;
+document.querySelectorAll(".volunteer-name").forEach(el => {
+    el.innerText = names[i];
+    i += 1;
+})
