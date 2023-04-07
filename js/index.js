036591cd3330af4971c8a7ae964694778d9257ed
Tom Wagg <twagg@college.harvard.edu>
2023-04-06 20:29:20 -0400

    add some coming soon notes, remove construction

--- js/index.js
+++ js/index.js
@@ -1,7 +1,3 @@
-// window.addEventListener("load", function() {
-//     window.location.href = "construction.html"
-// })
-
 const containers = document.querySelectorAll(".slanted-container");
 for (let i = 0; i < containers.length; i++) {
     containers[i].style.transform = "translateY(calc(-0.25 * " + i.toString() + "* var(--skewness))";
