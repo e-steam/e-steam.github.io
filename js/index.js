5f16dd5b03b577a103cc1cbaef94af82ac4d26e0
Tom Wagg <tomjwagg@gmail.com>
2023-04-24 22:26:41 -0700

    add navigation thing

--- js/index.js
+++ js/index.js
@@ -3,6 +3,12 @@ for (let i = 0; i < containers.length; i++) {
     containers[i].style.transform = "translateY(calc(-0.25 * " + i.toString() + "* var(--skewness))";
 }
 
+document.querySelectorAll("#menu a").forEach(el => {
+    el.addEventListener("click", function() {
+        document.getElementById("menu-toggler").click();
+    });
+})
+
 let resources = document.getElementById("resource-carousel");
 resources.addEventListener("slide.bs.carousel", function (e) {
     document.querySelector(".resource-toggle.active").classList.remove("active");
